cbe58a83754cd3b7a33512961f70330aaf7d37f0
Dike Mahoko <dike@uzinduziafrica.com>
2025-05-14 10:27:11 +0200

    backend update

--- backend/urls.py
+++ backend/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('api/',include('djoser.urls')),
     path('api/',include('users.urls')),
---
+++ mail/sendgrid_backend.py
@@ -0,0 +1,23 @@
+import os
+from django.core.mail.backends.base import BaseEmailBackend
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+class SendGridEmailBackend(BaseEmailBackend):
+    def send_messages(self, email_messages):
+        sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+        for message in email_messages:
+            mail = Mail(
+                from_email=message.from_email,
+                to_emails=message.to,
+                subject=message.subject,
+                plain_text_content=message.body,
+                html_content=message.alternatives[0][0] if message.alternatives else None,
+            )
+            try:
+                sg.send(mail)
+            except Exception as e:
+                if not self.fail_silently:
+                    raise
+        return len(email_messages)
