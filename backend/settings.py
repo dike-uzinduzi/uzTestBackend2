bf9e69043dff466d79b277881bb3f8760f7f7240
Dike Mahoko <h220278f@hit.ac.zw>
2025-08-29 16:00:23 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', 'False').lower() == 'true'
 # api/settings.py
-ALLOWED_HOSTS = ['uztestbackend2.onrender.com','onrender.com']
+ALLOWED_HOSTS = ['uztestbackend2.onrender.com']
 
 # -----------------------------
 # 📌 DJANGO REST FRAMEWORK
