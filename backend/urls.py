e58db107007ff570d187e6f00dac9bb743cc68b7
Dike Mahoko <h220278f@hit.ac.zw>
2025-08-29 16:32:47 +0200

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('api/',include('users.urls')),
     path('api/',include('albums.urls')),
     path('api/',include('payments.urls')),
-    path('api/',include('artists.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
